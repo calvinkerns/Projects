fa6a47ac8ba9a5d422e6227dd45dafac20e7bb85
calvinkerns <kernsc@wwu.edu>
2026-09-23 22:30:50 -0700

    Point the site at the deployed community API

--- bot-arena/site/js/config.js
+++ bot-arena/site/js/config.js
@@ -1,3 +1,3 @@
 // Where community bots are stored. Empty turns the community features off.
 // Set it with: node tools/set-api.mjs https://surge-api.<you>.workers.dev
-export const COMMUNITY_API = '';
+export const COMMUNITY_API = 'https://surge-api.calvinkerns.workers.dev';
